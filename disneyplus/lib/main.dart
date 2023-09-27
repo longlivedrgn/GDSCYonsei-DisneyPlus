53125b33ae9d90ce0d07bf35f0ec3a73851133a2
longlivedrgn <kimye9808@naver.com>
2023-09-27 21:06:05 +0900

    feat: Category 구현 및 Text 위젯 설정

--- disneyplus/lib/main.dart
+++ disneyplus/lib/main.dart
@@ -51,20 +51,21 @@ class Category extends StatelessWidget {
       width: width,
       margin: const EdgeInsets.symmetric(horizontal: 1.0),
       decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.white,
-            width: 1.0,
-          ),
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(3.0),
-            topRight: Radius.circular(3.0),
-            bottomRight: Radius.circular(3.0),
-            bottomLeft: Radius.circular(3.0),
-          ),
-          image: DecorationImage(
-              image: Image.asset(imageAssetPath).image,
-              fit: BoxFit.contain) // 정확히 뭔지는 찾아보기!...
-          ),
+        border: Border.all(
+          color: Colors.white,
+          width: 1.0,
+        ),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(3.0),
+          topRight: Radius.circular(3.0),
+          bottomRight: Radius.circular(3.0),
+          bottomLeft: Radius.circular(3.0),
+        ),
+        image: DecorationImage(
+          image: Image.asset(imageAssetPath).image,
+          fit: BoxFit.fill,
+        ), // 정확히 뭔지는 찾아보기!...
+      ),
     );
   }
 }
@@ -87,6 +88,99 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+// MediaQuery.of(context).size.width
+  CarouselSlider mainMoviesCarouselSlider() {
+    return CarouselSlider(
+      items: thumbNailItem.map(
+        (item) {
+          return Builder(
+            builder: (BuildContext context) {
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                // margin: const EdgeInsets.symmetric(horizontal: 10),
+                // decoration: const BoxDecoration(color: Colors.amber),
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Image.asset(item),
+                ),
+              );
+            },
+          );
+        },
+      ).toList(),
+      options: CarouselOptions(
+        // // Set the height of each carousel item
+        // height: 400,
+
+        // Set the size of each carousel item
+        // if height is not specified
+        // ✅ 아래와 같이 aspect ratio를 맞췄는데도 ratio가 제대로 맞춰서 나오지 않는다.
+        aspectRatio: 16 / 9,
+
+        // Set how much space current item widget
+        // will occupy from current page view
+        //
+        // viewportFraction: 0.8,
+
+        // Set the initial page
+        initialPage: 0,
+
+        // Set carousel to repeat when reaching the end
+        enableInfiniteScroll: true,
+
+        // Set carousel to scroll in opposite direction
+        reverse: false,
+
+        // Set carousel to display next page automatically
+        autoPlay: true,
+
+        // Set the duration of which carousel slider will wait
+        // in current page utill it moves on to the next
+        autoPlayInterval: const Duration(seconds: 3),
+
+        // // Set the duration of carousel slider
+        // // scrolling to the next page
+        // autoPlayAnimationDuration: const Duration(milliseconds: 800),
+
+        // Set the carousel slider animation
+        autoPlayCurve: Curves.fastOutSlowIn,
+
+        // Set the current page to be displayed
+        // bigger than previous or next page
+        enlargeCenterPage: true,
+
+        // Do actions for each page change
+        onPageChanged: (index, reason) {},
+
+        // Set the scroll direction
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+
+  // categories,
+  // Category(
+  //     width: MediaQuery.of(context).size.width * 0.28,
+  //     imageAssetPath: "Assets/CarouselImage1.jpg"),
+  // Category(
+  //     width: MediaQuery.of(context).size.width * 0.28,
+  //     imageAssetPath: "Assets/CarouselImage1.jpg"),
+  // Category(
+  //     width: MediaQuery.of(context).size.width * 0.28,
+  //     imageAssetPath: "Assets/CarouselImage1.jpg"),
+  Container categoriesContainer(List<Category> categories) {
+    return Container(
+      height: MediaQuery.of(context).size.width * 0.15,
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.05),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: categories,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // MediaQueryData deviceData = MediaQuery.of(context);
@@ -103,110 +197,54 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Container(
+        height: double.infinity,
         color: Theme.of(context).primaryColor,
         child: SingleChildScrollView(
           child: Column(
             children: [
-              CarouselSlider(
-                items: thumbNailItem.map(
-                  (item) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          // margin: const EdgeInsets.symmetric(horizontal: 10),
-                          // decoration: const BoxDecoration(color: Colors.amber),
-                          child: AspectRatio(
-                            aspectRatio: 16 / 9,
-                            child: Image.asset(item),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ).toList(),
-                options: CarouselOptions(
-                  // // Set the height of each carousel item
-                  // height: 400,
-
-                  // Set the size of each carousel item
-                  // if height is not specified
-                  // ✅ 아래와 같이 aspect ratio를 맞췄는데도 ratio가 제대로 맞춰서 나오지 않는다.
-                  aspectRatio: 16 / 9,
-
-                  // Set how much space current item widget
-                  // will occupy from current page view
-                  //
-                  // viewportFraction: 0.8,
-
-                  // Set the initial page
-                  initialPage: 0,
-
-                  // Set carousel to repeat when reaching the end
-                  enableInfiniteScroll: true,
-
-                  // Set carousel to scroll in opposite direction
-                  reverse: false,
-
-                  // Set carousel to display next page automatically
-                  autoPlay: true,
-
-                  // Set the duration of which carousel slider will wait
-                  // in current page utill it moves on to the next
-                  autoPlayInterval: const Duration(seconds: 3),
-
-                  // // Set the duration of carousel slider
-                  // // scrolling to the next page
-                  // autoPlayAnimationDuration: const Duration(milliseconds: 800),
-
-                  // Set the carousel slider animation
-                  autoPlayCurve: Curves.fastOutSlowIn,
-
-                  // Set the current page to be displayed
-                  // bigger than previous or next page
-                  enlargeCenterPage: true,
-
-                  // Do actions for each page change
-                  onPageChanged: (index, reason) {},
-
-                  // Set the scroll direction
-                  scrollDirection: Axis.horizontal,
-                ),
+              mainMoviesCarouselSlider(),
+              categoriesContainer(
+                [
+                  Category(
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      imageAssetPath: "Assets/CarouselImage1.jpg"),
+                  Category(
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      imageAssetPath: "Assets/CarouselImage1.jpg"),
+                  Category(
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      imageAssetPath: "Assets/CarouselImage1.jpg"),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.03,
+              ),
+              categoriesContainer(
+                [
+                  Category(
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      imageAssetPath: "Assets/CarouselImage1.jpg"),
+                  Category(
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      imageAssetPath: "Assets/CarouselImage1.jpg"),
+                  Category(
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      imageAssetPath: "Assets/CarouselImage1.jpg"),
+                ],
               ),
               Container(
-                height: MediaQuery.of(context).size.width * 0.15,
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: const Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    // Categories("assets/images/categories/disney.png"),
-                    // Categories("assets/images/categories/pixar.png"),
-                    // Categories("assets/images/categories/starwars.png")
-                  ],
+                margin: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.06,
+                  top: MediaQuery.of(context).size.width * 0.05,
                 ),
+                alignment: Alignment
+                    .centerLeft, // Align however you like (i.e .centerRight, centerLeft)
+                child: const Text("디즈니+ 최신작",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
               ),
-              // Container(
-              //   color: Colors.black,
-              //   width: 100,
-              //   height: 300,
-              // ),
-
-              // GridView.count(
-              //   crossAxisCount: 3,
-              //   childAspectRatio: 2 / 1,
-              //   mainAxisSpacing: 0, // 수평 Padding
-              //   crossAxisSpacing: 0, // 수직 Padding
-              //   children: [
-              //     Container(color: Colors.amber),
-              //     Container(color: Colors.amber),
-              //     Container(color: Colors.amber),
-              //     Container(color: Colors.amber),
-              //     Container(color: Colors.amber),
-              //     Container(color: Colors.amber),
-              //     Container(color: Colors.amber),
-              //   ],
-              // )
             ],
           ),
         ),
