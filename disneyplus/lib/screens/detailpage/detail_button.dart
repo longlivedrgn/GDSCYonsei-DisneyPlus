2163f28b0776e45ad176dd45db04f4f8a67e377b
longlivedrgn <kimye9808@naver.com>
2023-11-04 20:04:14 +0900

    feat: detailpage modal 구현하기

--- disneyplus/lib/screens/detailpage/detail_button.dart
+++ disneyplus/lib/screens/detailpage/detail_button.dart
@@ -5,16 +5,23 @@ class DetailButton extends StatelessWidget {
   final IconData icon;
   // ✅ button이 movie 값을 가지고 있는 것은 부자연스럽다!..
   final String? movieURL;
+  final VoidCallback triggerAction;
 
   const DetailButton(
-      {super.key, required this.text, required this.icon, this.movieURL});
+      {super.key,
+      required this.text,
+      required this.icon,
+      this.movieURL,
+      required this.triggerAction});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            triggerAction();
+          },
           icon: Icon(
             icon,
             size: 40,
--- disneyplus/lib/screens/detailpage/detailpage.dart
+++ disneyplus/lib/screens/detailpage/detailpage.dart
@@ -2,9 +2,129 @@ import 'package:disneyplus/screens/detailpage/detail_button.dart';
 import 'package:flutter/material.dart';
 import 'models/movie_information.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   final MovieInformation movieInformation;
   const DetailPage({required this.movieInformation, super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  bool isSaved = false;
+
+  Container saveDetailModal() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.3, // 모달 높이 크기
+      decoration: const BoxDecoration(
+        color: Color(0xff131a4a),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(0), // 모달 좌상단 라운딩 처리
+          topRight: Radius.circular(0), // 모달 우상단 라운딩 처리
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    width: 0.4,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.1,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  widget.movieInformation.name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+              child: const Row(
+                children: [
+                  IconTheme(
+                    data: IconThemeData(
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                    child: Icon(Icons.play_arrow),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ), // give it width
+
+                  Text(
+                    "재생",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isSaved = false;
+                });
+                Navigator.pop(context);
+              },
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: const Row(
+                  children: [
+                    IconTheme(
+                      data: IconThemeData(
+                        color: Colors.white,
+                        size: 26,
+                      ),
+                      child: Icon(Icons.delete),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "저장삭제",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void updateSaveButton() {
+    setState(() {
+      if (isSaved == false) {
+        isSaved = !isSaved;
+      } else {
+        showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) {
+            return saveDetailModal();
+          },
+        );
+      }
+    });
+  }
 
   Container moviePosterImage(double width, double height) {
     return Container(
@@ -15,7 +135,7 @@ class DetailPage extends StatelessWidget {
       // ),
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage(movieInformation.imageURL),
+          image: AssetImage(widget.movieInformation.imageURL),
           fit: BoxFit.cover,
         ),
       ),
@@ -45,7 +165,7 @@ class DetailPage extends StatelessWidget {
                 height: MediaQuery.of(context).size.height * 0.01,
               ),
               Text(
-                "${movieInformation.openDate} • ${movieInformation.runningTime} • ${movieInformation.genre}",
+                "${widget.movieInformation.openDate} • ${widget.movieInformation.runningTime} • ${widget.movieInformation.genre}",
                 style: const TextStyle(color: Colors.grey, fontSize: 17),
               ),
               SizedBox(
@@ -69,22 +189,25 @@ class DetailPage extends StatelessWidget {
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.01,
               ),
-              const Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   DetailButton(
                     text: "관심 콘텐츠",
                     icon: Icons.add,
+                    triggerAction: updateSaveButton,
                   ),
-                  SizedBox(width: 50), // give it width
+                  const SizedBox(width: 50), // give it width
                   DetailButton(
                     text: "예고편",
                     icon: Icons.movie,
+                    triggerAction: updateSaveButton,
                   ),
-                  SizedBox(width: 50), // give it width
+                  const SizedBox(width: 50), // give it width
                   DetailButton(
                     text: "저장",
-                    icon: Icons.download,
+                    icon: isSaved ? Icons.download_done : Icons.download,
+                    triggerAction: updateSaveButton,
                   )
                 ],
               ),
@@ -94,7 +217,7 @@ class DetailPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
                 child: Text(
-                  movieInformation.summary,
+                  widget.movieInformation.summary,
                   style: const TextStyle(color: Colors.white, fontSize: 17),
                 ),
               ),
